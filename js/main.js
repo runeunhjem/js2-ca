de3ee37f29aa1f8eca33ab5ee3eab32624a6f0f8
runeunhjem <runeunhjem@gmail.com>
2023-10-03 12:03:49 +0200

    forgot }

--- js/main.js
+++ js/main.js
@@ -39,10 +39,11 @@ if (profileImageElement) {
     // Set a default avatar URL if loggedInUserData or 'avatar' property is missing
     setTimeout(() => {
       if (loggedInUserData && loggedInUserData.avatar) {
-    // Set the "src" attribute to the avatar URL from loggedInUserData
-    profileImageElement.src = loggedInUserData.avatar;
-  } else {
-      profileImageElement.src = "https://t4.ftcdn.net/jpg/00/97/00/09/360_F_97000908_wwH2goIihwrMoeV9QF3BW6HtpsVFaNVM.jpg";
+        // Set the "src" attribute to the avatar URL from loggedInUserData
+        profileImageElement.src = loggedInUserData.avatar;
+      } else {
+        profileImageElement.src = "https://t4.ftcdn.net/jpg/00/97/00/09/360_F_97000908_wwH2goIihwrMoeV9QF3BW6HtpsVFaNVM.jpg";
+      }
     }, 1000);
   }
 }
