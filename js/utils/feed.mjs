43c248dfbb9511cc09a97348bd11c4bfd3a00452
runeunhjem <runeunhjem@gmail.com>
2023-10-06 23:13:21 +0200

    NO NULL ERROR

--- js/utils/feed.mjs
+++ js/utils/feed.mjs
@@ -215,9 +215,9 @@ export function createPostCard(post) {
   postMedia.classList.add("card-media", "m-1", "p-2", "rounded", "shadow");
 
   // Set the default placeholder image
-  const placeholderImage = `https://t4.ftcdn.net/jpg/00/97/00/09/360_F_97000908_wwH2goIihwrMoeV9QF3BW6HtpsVFaNVM.jpg`;
+  // const placeholderImage = `https://t4.ftcdn.net/jpg/00/97/00/09/360_F_97000908_wwH2goIihwrMoeV9QF3BW6HtpsVFaNVM.jpg`;
 
-  postMedia.src = post.media || placeholderImage; // Use post.media if available, otherwise use the placeholder
+  // postMedia.src = placeholderImage || post.media; // Use post.media if available, otherwise use the placeholder
   postMedia.alt = "Post Image"; // Set the image alt attribute
   postMedia.style.width = "100%"; // Set the image width
   // postMedia.style.height = "50%"; // Set the image width
