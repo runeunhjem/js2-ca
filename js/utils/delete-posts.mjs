092d383bc3d156e0a15ae660861fe9615286e411
runeunhjem <runeunhjem@gmail.com>
2023-10-14 08:15:14 +0200

    clean delete-posts

--- js/utils/delete-posts.mjs
+++ js/utils/delete-posts.mjs
@@ -1,12 +1,13 @@
 import { API_BASE_URL, allPostsTags, loggedInUser, deletePostOptions } from "../variables/consts.mjs";
 import { populateTagsSelector, filterUserTagsSelector } from "../feed-get-posts.js";
-// Function to delete a post
+
+// Delete a post
 export async function deletePost(postId) {
-  // const postId = localStorage.getItem("postId");
   const authorName = localStorage.getItem("authorName");
+
   // Check if the logged-in user matches the username from the URL
   if (loggedInUser === authorName) {
-    const deleteURL = `${API_BASE_URL}/social/posts/${postId}`; // Include postId in the URL
+    const deleteURL = `${API_BASE_URL}/social/posts/${postId}`;
     try {
       const response = await fetch(deleteURL, deletePostOptions);
       const json = await response.json();
@@ -14,7 +15,6 @@ export async function deletePost(postId) {
       if (response.status >= 200 && response.status <= 299) {
         populateTagsSelector(allPostsTags, filterUserTagsSelector);
         window.location.reload();
-
         return json;
       } else {
         // Display a message indicating that only the post owner can delete the post
