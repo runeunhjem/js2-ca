6dd358a98068a55a2b1a2083fa3744522f3fb5dc
runeunhjem <runeunhjem@gmail.com>
2023-10-15 06:01:39 +0200

    Add visual feedback and js doc to create new post

--- js/feed-add-post.js
+++ js/feed-add-post.js
@@ -2,8 +2,18 @@ import { createPostURL, postForm, addNewPostOptions, postsURL, fetchOptions } fr
 import { createNewPost } from "./utils/create-new-post.mjs";
 import { getFeedPostsWithToken } from "./feed-get-posts.js";
 
+const submitButton = document.getElementById("create-new-post");
+
 /**
- * Handles the submission of a new post form, creates a new post, and refreshes the feed.
+ * Event handler for the submission of a new post form. This function performs the following actions:
+ * 1. Prevents the default form submission behavior.
+ * 2. Retrieves user input data from form fields.
+ * 3. Constructs the data for creating a new post.
+ * 4. Submits the new post data to the server.
+ * 5. Updates the appearance of the submission button to indicate success.
+ * 6. Refreshes the feed to display the newly added post.
+ * 7. Resets the form fields and reverts the button to its original state after a delay.
+ *
  * @param {Event} event - The submit event object.
  * @returns {Promise<void>}
  */
@@ -27,9 +37,25 @@ postForm.addEventListener("submit", async function (event) {
   // Wait for new post to complete before fetching posts again
   await createNewPost(createPostURL, addNewPostOptions);
 
+  submitButton.classList.remove("btn-warning"); // Remove the default Bootstrap primary class
+  submitButton.classList.add("btn-success"); // Add the Bootstrap success class for green background
+  submitButton.classList.add("text-white"); // Add text-white class for white text
+
   // Refresh the feed posts to include the newly added postv
   await getFeedPostsWithToken(postsURL, fetchOptions);
+  setTimeout(() => {
+    // Reset the button to its original state
+    submitButton.classList.remove("btn-success");
+    submitButton.classList.remove("text-white");
+    submitButton.classList.add("btn-warning");
+    submitButton.classList.add("text-dark");
+    submitButton.disabled = false;
+
+    // Hide the form
+    const createPostForm = document.getElementById("createPostForm");
+    createPostForm.classList.remove("show");
+  }, 1200); // Reset after 2 seconds (adjust as needed)
 
   postForm.reset();
-
 });
+
