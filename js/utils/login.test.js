8d6ead5fc9ec52bc9c12240e228ad1008ad06973
Tokle89 <fredriktokle@gmail.com>
2023-10-31 19:10:32 +0100

    creates test for loginUser function

---
+++ js/utils/login.test.js
@@ -0,0 +1,40 @@
+import { loginUser } from "./login.mjs";
+
+const mockResponse = jest.fn().mockResolvedValue({
+  status: 200,
+  json: () =>
+    Promise.resolve({
+      accessToken: "mockAccessToken",
+    }),
+});
+
+global.fetch = mockResponse;
+
+describe("loginUser", () => {
+  beforeEach(() => {
+    global.localStorage = {
+      setItem: jest.fn(),
+      getItem: jest.fn(),
+      removeItem: jest.fn(),
+      clear: jest.fn(),
+    };
+  });
+
+  it("should store the access token in local storage upon successful login", async () => {
+    await loginUser();
+
+    expect(localStorage.setItem).toHaveBeenCalledWith("accessToken", mockAccessToken);
+  });
+
+  it("should not store the access token in local storage upon failed login", async () => {
+    const mockResponseError = {
+      status: 401,
+    };
+
+    global.fetch = mockResponseError;
+
+    await loginUser();
+
+    expect(localStorage.setItem).not.toHaveBeenCalled();
+  });
+});
