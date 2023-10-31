59b2e996d93cd1254ae5208cb634a49755458fbd
Tokle89 <fredriktokle@gmail.com>
2023-10-31 19:24:05 +0100

    clear localStorage after each test

--- js/utils/login.test.js
+++ js/utils/login.test.js
@@ -1,10 +1,11 @@
 import { loginUser } from "./login.mjs";
 
+const mockAccessToken = "mockAccessToken";
 const mockResponse = jest.fn().mockResolvedValue({
   status: 200,
   json: () =>
     Promise.resolve({
-      accessToken: "mockAccessToken",
+      accessToken: mockAccessToken,
     }),
 });
 
@@ -18,6 +19,10 @@ describe("loginUser", () => {
       removeItem: jest.fn(),
       clear: jest.fn(),
     };
+  });
+
+  afterEach(() => {
+    localStorage.clear();
   });
 
   it("should store the access token in local storage upon successful login", async () => {
