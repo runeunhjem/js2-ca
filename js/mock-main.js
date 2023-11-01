3ab730bf886400c26bcfaf5e31c5268b43167a9c
Tokle89 <fredriktokle@gmail.com>
2023-11-01 19:18:54 +0100

    adjusted comment and finished jest testing

--- js/mock-main.js
+++ js/mock-main.js
@@ -1,5 +1,5 @@
 // your-script-mock.js
-// mock of the logout eventlistener
+// mock of the logout eventlistener because the original nameless function could not be imported
 
 export const mockLogOut = () => {
   localStorage.clear();
