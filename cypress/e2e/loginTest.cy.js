576788c750ce4c78244a041ed53ec470c46a0f77
Tokle89 <fredriktokle@gmail.com>
2023-11-03 16:38:20 +0100

    updates url in login test

--- cypress/e2e/loginTest.cy.js
+++ cypress/e2e/loginTest.cy.js
@@ -1,6 +1,6 @@
 describe("login test", () => {
   it("logs in, and let user access the profile page", () => {
-    cy.visit("http://localhost:5501");
+    cy.visit("./index.html");
     cy.get("#loginEmail").type("tokle@noroff.no");
     cy.get("#loginPassword").type("fingern234");
     cy.get("#login-button").click();
