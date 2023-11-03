2c13a361a9feeb14397f39f57202b5d957f12aa7
Tokle89 <fredriktokle@gmail.com>
2023-11-03 17:24:46 +0100

    adds logout cypress test

---
+++ cypress/e2e/logout.cy.js
@@ -0,0 +1,17 @@
+describe("login test", () => {
+  it("logs in, and let user access the profile page", () => {
+    cy.visit("./index.html");
+    cy.get("#loginEmail").type("tokle@noroff.no");
+    cy.get("#loginPassword").type("fingern234");
+    cy.get("#login-button").click();
+
+    cy.wait(2000);
+
+    cy.url().should("include", "/profile");
+
+    cy.get(".logout-link").click();
+
+    cy.wait(2000);
+    cy.url().should("include", "/index.html");
+  });
+});
